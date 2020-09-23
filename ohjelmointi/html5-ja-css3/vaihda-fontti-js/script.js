a20adece87ddf6bf3d123184852c31ec62f83e48
Batista Ralitsa <s2000574@edu.bc.fi>
2020-09-23 16:11:45 +0300

    k,ncvlsdnkresdx

---
+++ ohjelmointi/html5-ja-css3/vaihda-fontti-js/script.js
@@ -0,0 +1,57 @@
+
+var size = 0;
+function changeFontSize(){
+    if (size == 6) {
+        size = 1;
+    }
+    console.log("changeFontSize, size: " + size);
+    size = size + 1;
+    lista = document.getElementById('lista');
+    if (size == 1) {
+        lista.style.fontSize = "20px";
+    }
+    else if (size = 2) {
+        lista.style.fontSoze = "24px";
+    }
+    else if (size =3) {
+        lista.style.fontSoze = "28px";
+    }
+    else if (size = 4) {
+        lista.style.fontSoze = "32px";
+    }
+    else if (size = 5) {
+        lista.style.fontSoze = "20px";
+    }
+
+}
+
+
+var type = 0;
+console.log("");
+function changeFontStyle() {
+    if(type == 6){
+        type = 1;
+    }
+    console.log("changeFontStyle, type: " + type);
+    type = type + 1;
+    lista = document.getElementById("lista");
+    if (type == 1) {
+        lista.style.fontFamily = "Roboto, sans-serif";
+    }
+
+    else if (type == 2) {
+        lista.style.fontFamily = "Mansalva, cursive";
+    }
+    
+    else if (type == 3) {
+        lista.style.fontFamily = "Grenze, serif";
+    }
+
+    else if (type == 4) {
+        lista.style.fontFamily = "Turret Road, cursive";
+    }
+
+    else if (type == 5) {
+        lista.style.fontFamily = "Courier New";
+    }
+}
