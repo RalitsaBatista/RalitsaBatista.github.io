dbbbab26b1d9f8e7a3d28823e655387df8cb49f8
Batista Ralitsa <s2000574@edu.bc.fi>
2020-11-25 15:10:33 +0200

    dknsfkueflwkefa

--- ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-perusteet/my-first-angular-app/src/app/app.module.ts
+++ ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-perusteet/my-first-angular-app/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { SharedModule } from './shared/shared.module';
+import { CustomersModule } from './customers/customers.module';
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, CustomersModule, SharedModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
