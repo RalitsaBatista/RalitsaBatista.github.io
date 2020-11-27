f101562985e25f46eafbadf2acbd2d2d50b72978
Batista Ralitsa <s2000574@edu.bc.fi>
2020-11-27 14:05:49 +0200

    kdfnsldfssssss

--- ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-perusteet/my-dream-app-component-templates/src/app/servers/servers.component.ts
+++ ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-perusteet/my-dream-app-component-templates/src/app/servers/servers.component.ts
@@ -2,12 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `<section>
-  <h1>I'm in the Servers Component</h1>
-  <p>Below this I have 2 Server Components</p>
-  <app-server></app-server>
-  <app-server></app-server>
-  <app-server></app-server><app-server></app-server></section>`, 
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 
