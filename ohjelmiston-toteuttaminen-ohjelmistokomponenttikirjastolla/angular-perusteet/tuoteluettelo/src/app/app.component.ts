a815c6a7ac142a78231ffad21b17eaaa633e143e
Batista Ralitsa <s2000574@edu.bc.fi>
2021-01-24 18:14:57 +0200

    rep

--- ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-perusteet/tuoteluettelo/src/app/app.component.ts
+++ ohjelmiston-toteuttaminen-ohjelmistokomponenttikirjastolla/angular-perusteet/tuoteluettelo/src/app/app.component.ts
@@ -1,5 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import phones from './app/puhelinten-tuotetiedot'
 
+
+type PhoneType = [{
+  id: number,
+  name: string,
+  additionalFeatures: string,
+  availability: string,
+  description:string,
+  sizeAndWeight: number,
+  storage: number,
+}]
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +18,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tuoteluettelo';
+
 }
+//OpenOptions() {
+
+  //let DataObject = {
+    //id: this.itemId,
+    //itemName: this.itemName,
+    //itemQuantity: this.itemQuantity,
+    //unit: this.unit,
+    //  }
+
+  //this.LinkToDataSharingService.ShareData(DataObject)
+
+  //this.LinkToDataSharingService.toggleOptions();
+ //}
+
+
+
